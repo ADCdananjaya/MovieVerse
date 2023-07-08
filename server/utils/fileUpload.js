059c9b189bdf4cfb6d449e95f8708bed8384046c
Chamod Dananjaya <chamoddananjaya49@gmail.com>
2023-07-08 12:38:56 +0530

    Add storage function for storing images.

--- server/utils/fileUpload.js
+++ server/utils/fileUpload.js
@@ -2,11 +2,12 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "../images/");
+    cb(null, "./images/");
   },
   filename: (req, file, cb) => {
+    const extension = file.originalname.split(".").pop();
     const suffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, file.fieldname + "-", suffix);
+    cb(null, file.fieldname + "-" + `${suffix}.${extension}`);
   },
 });
 
